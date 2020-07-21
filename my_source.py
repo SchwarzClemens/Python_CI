c642fc59919680b4a4206e3dc08167dfcac943e1
SchwarzClemens <clemens.schwarz1@gmx.de>
2020-07-21 16:30:54 +0200

    Inital commit

---
+++ my_source.py
@@ -0,0 +1,11 @@
+"""Contains code under test for solution_ci."""
+
+def add_func(a, var_two):
+    """
+    Add two variables.
+    Args
+        a: first variable
+        var_two: second variable
+    """
+    return a + var_two
+
