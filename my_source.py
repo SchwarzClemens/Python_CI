8dfcc46f43075424558972fb6b2f1ed888a67409
SchwarzClemens <clemens.schwarz1@gmx.de>
2020-07-21 18:26:45 +0200

    Docstring von add_func angepasst

--- my_source.py
+++ my_source.py
@@ -6,7 +6,7 @@ def add_func(var_one, var_two):
     Add two variables.
 
     Args
-        var_one: first variable of add_func
+        var_one: first variable of add_funcs
         var_two: second variable of add_funcs
     """
     return var_one + var_two
